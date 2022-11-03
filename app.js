b6f09f02c8c686620c231255bc44efde61daa8a6
Mateusz Chmielewski <chmielek1924@gmail.com>
2022-11-03 01:04:01 +0100

    Upload na gita

---
+++ app.js
@@ -0,0 +1,28 @@
+const searchField = document.getElementById("filter-field");
+const searchBtn = document.getElementById("search-btn");
+const characterList = document.getElementById("lista");
+
+async function getData() {
+  url = "https://rickandmortyapi.com/api/character";
+  const data = fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      createFiltredArray(res, searchField);
+    });
+}
+
+function createFiltredArray(arr, inputValue) {
+  characterList.innerHTML = "";
+  for (const hero of arr.results) {
+    if (hero.name.includes(inputValue.value)) {
+      renderList(hero);
+    }
+  }
+}
+function renderList(character) {
+  const newCharacter = document.createElement("li");
+  newCharacter.textContent = character.name;
+  lista.append(newCharacter);
+}
+
+searchBtn.addEventListener("click", getData);
