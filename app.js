8d624a1c620c9fa4461ed01d0f3658b6031d90d6
Mateusz Chmielewski <chmielek1924@gmail.com>
2022-11-03 13:08:31 +0100

    withDestructor

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const searchField = document.getElementById("filter-field");
-const searchBtn = document.getElementById("search-btn");
 const characterList = document.getElementById("lista");
-
+const newCharacterName = document.createElement("li");
+const searchBtn = document.getElementById("search-btn");
+const searchField = document.getElementById("filter-field");
 async function getData() {
   url = "https://rickandmortyapi.com/api/character";
   const data = fetch(url)
     .then((res) => res.json())
     .then((res) => {
-      createFiltredArray(res, searchField);
+      characterList.innerHTML = "";
+      const { info, results } = res;
+      for (const character of results) {
+        if (character.name.includes(searchField.value)) {
+          const newCharacterName = document.createElement("li");
+          newCharacterName.textContent = character.name;
+          characterList.append(newCharacterName);
+        }
+      }
     });
 }
-
-function createFiltredArray(arr, inputValue) {
-  characterList.innerHTML = "";
-  for (const hero of arr.results) {
-    if (hero.name.includes(inputValue.value)) {
-      renderList(hero);
-    }
-  }
-}
-function renderList(character) {
-  const newCharacter = document.createElement("li");
-  newCharacter.textContent = character.name;
-  lista.append(newCharacter);
-}
-console.log("chuj");
-
 searchBtn.addEventListener("click", getData);
